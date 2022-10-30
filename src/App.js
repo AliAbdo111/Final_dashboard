099a04403fd98be1aa1e2c62a92eec39b7d6620e
Aboalhassan Abdo <93014883+AliAbdo111@users.noreply.github.com>
2022-10-30 11:14:04 +0200

    Add files via upload
    hello darsh

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./Pages/Dashboard";
+import LoginPAge from "./Pages/login";
+import Sidbar from "./component/sidbar/Sidbar";
+function App() {
+  let token = localStorage.getItem("token");
+  return (
+    <>
+    <Sidbar/>
+    <Routes>
+    <Route path="/" element={<LoginPAge />} />
+    <Route path="/Dashboard" element={<Dashboard />}></Route>
+    <Route path="*" element={< Dashboard/>} /> 
+  </Routes>
+  </>
+  );
+}
+
+export default App;
